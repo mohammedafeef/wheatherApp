e042f819199cbe4c7b0c5ed1c8f5f1df59a6bfb4
mohammedafeef <mohammedafeef.maf@gmail.com>
2020-12-16 13:49:20 +0530

    all bugs are fixed

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const https = require("https");
+const http = require("http");
 const fs = require("fs");
+const { json } = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
-const weatherPage = readFileSync('wheatherUI.html','utf-8');
+const weatherPage = fs.readFileSync('wheatherUI.html','utf-8');
 const changeTheContent = (page,weatherInfo)=>{
-    const imageIcon = `http://openweathermap.org/img/wn/${weatherInfo.weather[0].icons}@2x.png`
-    const tempPage = page.replace("icons/cloud.png",imageIcon)
+    const imageIcon = `http://openweathermap.org/img/wn/${weatherInfo.weather[0].icon}@2x.png`
+    console.log(weatherInfo.weather[0].icons,weatherInfo);
+    let tempPage = page.replace("icons/cloud.png",imageIcon)
     tempPage = tempPage.replace("Kochi",weatherInfo.name)
     tempPage = tempPage.replace("23",weatherInfo.main.temp);
     tempPage = tempPage.replace("22",weatherInfo.main.temp_min);
     tempPage = tempPage.replace("25",weatherInfo.main.temp_max);
+    console.log(tempPage);
     return tempPage;
 }
 app.get("/",(req,res)=>{
     res.sendFile(`${__dirname}/wheatherUI.html`,(err)=>{
-        err?console.log(err):console.log("sended sueccsesfully");
+        err?console.log("error is console logged"+err):console.log("sended sueccsesfully");
     })
 })
 app.post("/",(req,res)=>{
+    console.log("post method is called")
     const place = req.body.place;
     const authToken ='0d376dcd44e835f8eb2eec02692a8e51' ;
     const url =`http://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=${authToken}`;
-    https.get(url,(response)=>{
+    http.get(url,(response)=>{
         response.on('data',(data)=>{
-            const weatherData = data.JSON();
+            const weatherData = JSON.parse(data);
             const weatherObj =[weatherData];
-            const updatedPage = weatherObj.map((data)=>changeTheContent(weatherPage)).add("");
+            const updatedPage = weatherObj.map((data)=>changeTheContent(weatherPage,data)).join('');
             res.write(updatedPage);
         })
     })
 })
-https.get()
-app.listen("3000",()=>{
+app.listen("4000",()=>{
     console.log("server is listening to port 3000..")
 });
